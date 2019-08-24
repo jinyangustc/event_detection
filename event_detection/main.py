2864559496c8bfbceaee0e323a15dfed91460ded
Jiawei <jiaweit2@illinois.edu>
2019-08-24 09:58:53 -0500

    make config, stopwords, input_str arguments to be parsed in

--- event_detection/main.py
+++ event_detection/main.py
@@ -108,23 +108,44 @@ def run(
     return timeline
 
 
+def print_usage() -> str:
+    usage = "\nRequire arguments: \n \
+            -c config file \n \
+            -s stop words file \n \
+            -i input corpus \n "
+    print(usage)
+    exit()
+
+
 if __name__ == "__main__":
     import json
     import toml
     import textwrap
     import time
+    import sys
+    import os
+    import getopt
 
-    config = toml.load("../data/config.toml")
+    optlist, _ = getopt.getopt(sys.argv[1:], "c:s:i:")
+    if len(optlist) < 3:
+        print_usage()
+    for argv in optlist:
+        if argv[1] == "":
+            print_usage()
+        elif not os.path.isfile(argv[1]):
+            raise ImportError("Error: file {} doesn't exist".format(argv[1]))
+
+    config = toml.load(optlist[0][1])
     significance_threshold = config["significance_threshold"]
     window_size = config["window_size"]
     similarity_threshold = config["similarity_threshold"]
     box_keepalive_time = datetime.timedelta(hours=config["box_keepalive_time"])
 
-    with open("../data/stopwords.txt") as f:
+    with open(optlist[1][1]) as f:
         stop_words = [x.strip() for x in f.readlines()]
 
     # Load input corpus
-    with open("../data/example.json") as f:
+    with open(optlist[2][1]) as f:
         input_strs = json.load(f)
 
     def print_fix_width(long_str):
