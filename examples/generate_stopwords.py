301ac881b9fec2c48c23c0869500dea0f9b34582
Jinyang Li <jinyang7@illinois.edu>
2019-09-20 13:47:43 -0500

    Add stopword TF-IDF threshold to the configuration file

--- examples/generate_stopwords.py
+++ examples/generate_stopwords.py
@@ -1,6 +1,7 @@
 import json
 
 import click
+import toml
 
 from event_detection.corpus import get_unimportant_words
 
@@ -8,15 +9,16 @@ from event_detection.corpus import get_unimportant_words
 @click.command()
 @click.argument("tweets_json", type=click.File("r"))
 @click.argument("stopwords_file", type=click.Path())
-@click.argument("tfidf_threshold", type=float)
-def generate_stopwords(tweets_json, stopwords_file, tfidf_threshold):
+@click.argument("config_file", type=click.Path())
+def generate_stopwords(tweets_json, stopwords_file, config_file: str):
     tweets = json.load(tweets_json)
     tweets = [x["content"] for x in tweets]
+    config = toml.load(config_file)
+    tfidf_threshold = config["stopword_tfidf_thresh"]
     stopwords = get_unimportant_words(tweets, tfidf_threshold)
     with open(stopwords_file, "w") as f:
         f.writelines("\n".join(sorted(stopwords)))
 
 
 if __name__ == "__main__":
-    # python generate_stopwords ../data/2019-07-1517.json stopwords.txt 0.05
     generate_stopwords()
